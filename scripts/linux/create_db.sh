8762f279c534dd92cd84d8174da7d40b1a2bf94b
Michał Oracki <michal.oracki.stud@pw.edu.pl>
2022-06-05 23:06:45 +0200

    Update create database script

--- scripts/linux/create_db.sh
+++ scripts/linux/create_db.sh
@@ -2,18 +2,29 @@
 
 sqlite3 zpr.db <<EOF
 PRAGMA foreign_keys = ON;
-create table Cameras (
+
+CREATE TABLE IF NOT EXISTS Simulations (
+    simulation_id INTEGER PRIMARY KEY,
+    name TEXT
+);
+CREATE TABLE IF NOT EXISTS Cameras (
     camera_id INTEGER PRIMARY KEY,
     x_coordinate REAL,
     y_coordinate REAL,
-    visibility_degree REAL
-    );
-create table Measurements (
+    visibility_degree REAL,
+    precision REAL CHECK (precision BETWEEN 0 AND 1)
+);
+CREATE TABLE IF NOT EXISTS Measurements (
     measurement_id INTEGER PRIMARY KEY, 
     vehicle_type INTEGER, 
     x_coordinate REAL, 
-    y_coordinate REAL, 
+    y_coordinate REAL,
+    certainty REAL, 
     camera_id INTEGER,
+    simulation_id INTEGER,
     FOREIGN KEY (camera_id)
-       REFERENCES Cameras (camera_id))
+       REFERENCES Cameras (camera_id),
+    FOREIGN KEY (simulation_id)
+       REFERENCES Simulations (simulation_id)
+);
 EOF
